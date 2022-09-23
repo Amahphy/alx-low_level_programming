b6717c5edea6db37a83a9086313a97a6e90ead37
Amah Orji <quantumamah@gmail.com>
2022-09-23 15:21:16 -0700

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main ()
+{
+	printf("_putchar");
+
+	return (0);
+}
